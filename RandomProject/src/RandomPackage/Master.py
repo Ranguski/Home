2cfecba981760c4c58966c7161007a05615e3b43
Ranguski <ranguski@example.com>
2024-04-28 16:45:17 +0530

    added Random project

---
+++ RandomProject/src/RandomPackage/Master.py
@@ -0,0 +1,23 @@
+import math;
+import random;
+
+class Master(object):
+    def __init__(self, firstValue, secondValue):
+        self.firstValue = firstValue;
+        self.secondValue = secondValue;
+
+    def add(self):
+        return (self.firstValue + self.second.Value);
+
+    def subtract(self):
+         return (self.firstValue - self.secondValue);
+
+    def product(self):
+        return (self.firstValue * self.secondValue);
+
+    def divider(self):
+        return (self.firstValue / self.secondValue);
+
+def fact(number):
+    if(number == 0):
+        return (1);
