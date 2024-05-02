899c5d15f6219c2d5f0c62db46b182b4a4f5f26c
Ranguski <ranguski@example.com>
2024-05-03 01:45:14 +0530

    added shellScript.sh

--- RandomProject/src/RandomPackage/Master.py
+++ RandomProject/src/RandomPackage/Master.py
@@ -1,13 +1,13 @@
-import math;
-import random;
+import json;
 
 class Master(object):
     def __init__(self, firstValue, secondValue):
+        self.json = dict();
         self.firstValue = firstValue;
         self.secondValue = secondValue;
 
     def add(self):
-        return (self.firstValue + self.second.Value);
+        return (self.firstValue + self.secondValue);
 
     def subtract(self):
          return (self.firstValue - self.secondValue);
@@ -18,6 +18,19 @@ class Master(object):
     def divider(self):
         return (self.firstValue / self.secondValue);
 
-def fact(number):
-    if(number == 0):
-        return (1);
+    def getJson(self):
+        return (self.json);
+
+    def main(self):
+        self.json["add"] = self.add();
+        self.json["subtract"] = self.subtract();
+        self.json["product"] = self.product();
+        self.json["divide"] = self.divider();
+
+
+if __name__ == "__main__":
+    obj = Master(20,10);
+    obj.main();
+
+    print(json.dumps(obj.getJson(), indent = 3));
+
